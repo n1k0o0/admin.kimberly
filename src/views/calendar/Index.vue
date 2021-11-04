d825644a4f79e1d012578c1969dae14efeb92b0d
Nicat Ismayilov <n1ko.net.dev@gmail.com>
2021-11-04 11:28:15 +0400

    1650 (fix) add date to schedule filter

--- src/views/calendar/Index.vue
+++ src/views/calendar/Index.vue
@@ -5,6 +5,25 @@
       <el-row
         :gutter="20"
       >
+        <el-col
+          :span="5"
+          class="pb-5"
+        >
+          <el-select
+            v-model="search.stadium_ids"
+            filterable
+            multiple
+            placeholder="Стадион"
+            @change="searchGames"
+          >
+            <el-option
+              v-for="(stadium) in stadiums"
+              :key="stadium.id"
+              :label="stadium.title"
+              :value="stadium.id"
+            />
+          </el-select>
+        </el-col>
         <el-col :span="5">
           <el-select
             v-model="search.tournament_ids"
@@ -71,21 +90,23 @@
             />
           </el-select>
         </el-col>
-        <el-col :span="4">
-          <el-select
-            v-model="search.stadium_ids"
-            filterable
-            multiple
-            placeholder="Стадион"
+        <el-col :span="5">
+          <el-date-picker
+            v-model="search.date_from"
+            format="DD-MM-YYYY"
+            placeholder="Дата начала матча"
+            type="date"
             @change="searchGames"
-          >
-            <el-option
-              v-for="(stadium) in stadiums"
-              :key="stadium.id"
-              :label="stadium.title"
-              :value="stadium.id"
-            />
-          </el-select>
+          />
+        </el-col>
+        <el-col :span="5">
+          <el-date-picker
+            v-model="search.date_to"
+            format="DD-MM-YYYY"
+            placeholder="Дата завершения матча"
+            type="date"
+            @change="searchGames"
+          />
         </el-col>
       </el-row>
       <el-row
@@ -124,25 +145,25 @@
 </template>
 
 <script>
-import { computed, onMounted, reactive, ref, watch } from 'vue'
-import { useLoadingState } from '@/composables/common/useLoadingState.js'
+import {computed, onMounted, reactive, ref, watch} from 'vue'
+import {useLoadingState} from '@/composables/common/useLoadingState.js'
 import usePagination from '@/composables/common/usePagination.js'
-import { paginateTournaments } from '@/services/tournaments/tournaments.js'
-import { paginateLeagues } from '@/services/leagues/leagueService.js'
-import { paginateStadiums } from '@/services/stadiums/stadiums.js'
-import { paginateGames, removeGame } from '@/services/games/gameService.js'
-import { getPrintableTournamentStatus } from '@/services/tournaments/Tournament.js'
-import { getTeams } from '@/services/schools/teams/teams.js'
+import {paginateTournaments} from '@/services/tournaments/tournaments.js'
+import {paginateLeagues} from '@/services/leagues/leagueService.js'
+import {paginateStadiums} from '@/services/stadiums/stadiums.js'
+import {paginateGames, removeGame} from '@/services/games/gameService.js'
+import {getPrintableTournamentStatus} from '@/services/tournaments/Tournament.js'
+import {getTeams} from '@/services/schools/teams/teams.js'
 import useCountryAndCity from '@/composables/useCountryAndCity.js'
 import Games from '@/components/games/Games.vue'
 
 export default {
   name: 'Index',
-  components: { Games },
-  setup () {
-    const { loading, setLoaded, setLoading } = useLoadingState(true)
-    const { pagination, setPagination, currentPage } = usePagination()
-    const { selectedCityId, selectedCountryId } = useCountryAndCity()
+  components: {Games},
+  setup() {
+    const {loading, setLoaded, setLoading} = useLoadingState(true)
+    const {pagination, setPagination, currentPage} = usePagination()
+    const {selectedCityId, selectedCountryId} = useCountryAndCity()
     const search = reactive({
       city_id: selectedCityId,
       country_id: selectedCountryId,
@@ -156,9 +177,9 @@ export default {
     onMounted(async () => {
       setLoading()
 
-      const { data: { data: tournamentItems } } = await paginateTournaments(search, null, 0)
-      const { data: { data: leagueItems } } = await paginateLeagues(search, null, 0)
-      const { data: { data: stadiumItems } } = await paginateStadiums(search, null, 0)
+      const {data: {data: tournamentItems}} = await paginateTournaments(search, null, 0)
+      const {data: {data: leagueItems}} = await paginateLeagues(search, null, 0)
+      const {data: {data: stadiumItems}} = await paginateStadiums(search, null, 0)
 
       await searchGames()
       leagues.value = leagueItems
@@ -170,7 +191,7 @@ export default {
 
     watch(
       () => search.league_ids,
-      (newName, prevName) => {
+      () => {
         if (search.league_ids && search.league_ids.length) {
 
           if (search.division_ids && search.division_ids.length) {
@@ -187,9 +208,9 @@ export default {
     )
     watch(
       () => search.division_ids,
-      async (newName, prevName) => {
+      async () => {
         if (search.division_ids && search.division_ids.length) {
-          const { data: { data: teamItems, } } = await getTeams(search, null, 0)
+          const {data: {data: teamItems,}} = await getTeams(search, null, 0)
           teams.value = teamItems
           if (search.team_ids && search.team_ids.length) {
             search.team_ids = search.team_ids.filter(function (el) {
@@ -205,10 +226,10 @@ export default {
     )
     watch(
       () => search.city_id,
-      async (newName, prevName) => {
-        const { data: { data: tournamentItems } } = await paginateTournaments(search, null, 0)
-        const { data: { data: leagueItems } } = await paginateLeagues(search, null, 0)
-        const { data: { data: stadiumItems } } = await paginateStadiums(search, null, 0)
+      async () => {
+        const {data: {data: tournamentItems}} = await paginateTournaments(search, null, 0)
+        const {data: {data: leagueItems}} = await paginateLeagues(search, null, 0)
+        const {data: {data: stadiumItems}} = await paginateStadiums(search, null, 0)
 
         leagues.value = leagueItems
         stadiums.value = stadiumItems
@@ -217,7 +238,7 @@ export default {
         search.stadium_ids = []
         search.tournament_ids = []
         games.value = []
-        setPagination({ current_page: 0, total: 0, per_page: 0 })
+        setPagination({current_page: 0, total: 0, per_page: 0})
         await searchGames()
       },
     )
@@ -227,7 +248,7 @@ export default {
       if (currentPage.value > 0) {
         try {
           setLoading()
-          const { data: { data: gameItems, meta } } = await paginateGames(search, currentPage.value)
+          const {data: {data: gameItems, meta}} = await paginateGames(search, currentPage.value)
           games.value = gameItems
           setPagination(meta)
         } catch (e) {
@@ -248,7 +269,7 @@ export default {
     const searchGames = async () => {
       try {
         setLoading()
-        const { data: { data: gameItems, meta } } = await paginateGames(search)
+        const {data: {data: gameItems, meta}} = await paginateGames(search)
         games.value = gameItems
         setPagination(meta)
       } catch (e) {
@@ -277,7 +298,7 @@ export default {
       try {
         setLoading()
         await removeGame(game.id)
-        const { data: { data: gameItems, meta } } = await paginateGames(search)
+        const {data: {data: gameItems, meta}} = await paginateGames(search)
         games.value = gameItems
         setPagination(meta)
       } catch (e) {
